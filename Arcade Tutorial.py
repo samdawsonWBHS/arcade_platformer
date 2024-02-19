29338b99c4f94df817c00ecbf18518b06084c95f
samdawsonWBHS <sd20428@my.westlake.school.nz>
2024-02-20 09:35:37 +1300

    Added sprite code to the platformer

--- Arcade Tutorial.py
+++ Arcade Tutorial.py
@@ -3,11 +3,14 @@ Platformer Game
 """
 import arcade
 
-# Constants
+# Constants used to size and name the game window
 SCREEN_WIDTH = 500
 SCREEN_HEIGHT = 325
 SCREEN_TITLE = "Sam's Platformer"
 
+# Constant used to adjust the size of sprites from their original size
+CHARACTER_SCALING = 1
+TILE_SCALING = 0.5
 
 class MyGame(arcade.Window):
     """
@@ -16,14 +19,50 @@ class MyGame(arcade.Window):
 
     def __init__(self):
 
-        # Call the parent class and set up the window
+        # Inherits the attributes of the parent class
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+
+        # Lists that contain all of the sprites
+        self.player_list = None
+        self.wall_list = None
+
+        # Separate variable that contains the player sprite
 
         arcade.set_background_color(arcade.csscolor.INDIGO)
 
     def setup(self):
         """Set up the game here. Call this function to restart the game."""
-        pass
+
+        # Create the sprite lists as objects of arcade library sprite classes - allows for manipulation of sprites as opposed to empty lists
+        self.player_list = arcade.SpriteList()
+        self.wall_list = arcade.SpriteList(use_spatial_hash=True)
+
+        """ # Set up the player, specifically placing it at these coordinates.
+        image_source = ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png"
+        self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
+        self.player_sprite.center_x = 64
+        self.player_sprite.center_y = 128
+        self.player_list.append(self.player_sprite)
+
+        # Create the ground
+        # This shows using a loop to place multiple sprites horizontally
+        for x in range(0, 1250, 64):
+            wall = arcade.Sprite(":resources:images/tiles/grassMid.png", TILE_SCALING)
+            wall.center_x = x
+            wall.center_y = 32
+            self.wall_list.append(wall)
+
+        # Put some crates on the ground
+        # This shows using a coordinate list to place sprites
+        coordinate_list = [[512, 96], [256, 96], [768, 96]]
+
+        for coordinate in coordinate_list:
+            # Add a crate on the ground
+            wall = arcade.Sprite(
+                ":resources:images/tiles/boxCrate_double.png", TILE_SCALING
+            )
+            wall.position = coordinate
+            self.wall_list.append(wall) """
 
     def on_draw(self):
         """Render the screen."""
