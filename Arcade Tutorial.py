82f26104bce01986185999043f6e3c1f610a88ee
samdawsonWBHS <sd20428@my.westlake.school.nz>
2024-02-21 15:11:07 +1300

    Completed Step 3 - Scene Object code

--- Arcade Tutorial.py
+++ Arcade Tutorial.py
@@ -1,14 +1,13 @@
 """
 Platformer Game
 """
+# Imports arcade library
 import arcade
 
-# Constants used to size and name the game window
-SCREEN_WIDTH = 500
-SCREEN_HEIGHT = 325
+# Constants
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 960
 SCREEN_TITLE = "Sam's Platformer"
-
-# Constant used to adjust the size of sprites from their original size
 CHARACTER_SCALING = 1
 TILE_SCALING = 0.5
 
@@ -19,64 +18,61 @@ class MyGame(arcade.Window):
 
     def __init__(self):
 
-        # Inherits the attributes of the parent class
+        # Inherits methods from parent class (Window)
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
-        # Lists that contain all of the sprites
-        self.player_list = None
-        self.wall_list = None
+        # Initialises scene and player_sprite attributes
+        self.scene = None
+        self.player_sprite = None
 
-        # Separate variable that contains the player sprite
-
+        # Sets background colour for the window using arcade's provided colours
         arcade.set_background_color(arcade.csscolor.INDIGO)
 
     def setup(self):
-        """Set up the game here. Call this function to restart the game."""
+        """Set up the game here. Call this method to restart the game."""
 
-        # Create the sprite lists as objects of arcade library sprite classes - allows for manipulation of sprites as opposed to empty lists
-        self.player_list = arcade.SpriteList()
-        self.wall_list = arcade.SpriteList(use_spatial_hash=True)
+        # Initialises the Scene class
+        self.scene = arcade.Scene()
 
-        """ # Set up the player, specifically placing it at these coordinates.
-        image_source = ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png"
+        # Adds "Player" anbd "Walls" sprite lists to the Scene class
+        self.scene.add_sprite_list("Player")
+        self.scene.add_sprite_list("Walls", use_spatial_hash=True) # Spatial hashing improves collision detection for stationary sprites
+
+        # Adds information for player sprite including image and position
+        image_source = ":resources:images/animated_characters/male_adventurer/maleAdventurer_idle.png"
         self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
-        self.player_sprite.center_x = 64
-        self.player_sprite.center_y = 128
-        self.player_list.append(self.player_sprite)
+        self.player_sprite.position = 64, 128
 
-        # Create the ground
-        # This shows using a loop to place multiple sprites horizontally
+        # Adds player sprite to "Player" sprite list
+        self.scene.add_sprite("Player", self.player_sprite)
+
         for x in range(0, 1250, 64):
             wall = arcade.Sprite(":resources:images/tiles/grassMid.png", TILE_SCALING)
             wall.center_x = x
             wall.center_y = 32
-            self.wall_list.append(wall)
+            self.scene.add_sprite("Walls", wall)
 
-        # Put some crates on the ground
-        # This shows using a coordinate list to place sprites
-        coordinate_list = [[512, 96], [256, 96], [768, 96]]
+        coordinate_list = [[512, 96], [256, 96], [768, 96], [576, 224], [640, 224], [704, 224], [768, 224]]
 
         for coordinate in coordinate_list:
-            # Add a crate on the ground
             wall = arcade.Sprite(
                 ":resources:images/tiles/boxCrate_double.png", TILE_SCALING
             )
             wall.position = coordinate
-            self.wall_list.append(wall) """
+            self.scene.add_sprite("Walls", wall)
 
     def on_draw(self):
         """Render the screen."""
 
         self.clear()
-        # Code to draw the screen goes here
 
+        self.scene.draw()
 
 def main():
     """Main function"""
     window = MyGame()
     window.setup()
     arcade.run()
-
 
 if __name__ == "__main__":
     main()
