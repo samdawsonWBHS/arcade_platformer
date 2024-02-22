343111939135e2e7f6df70db0fb43026e63cee00
samdawsonWBHS <sd20428@my.westlake.school.nz>
2024-02-23 12:51:44 +1300

    Completed step 6

--- Arcade Tutorial.py
+++ Arcade Tutorial.py
@@ -24,10 +24,11 @@ class MyGame(arcade.Window):
         # Inherits methods from parent class (Window)
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
-        # Initialises scene, player_sprite, and physics_engine attributes
-        self.scene = None
+        # Initialises scene, player_sprite, camera, and physics_engine attributes
+        """self.scene = None
         self.player_sprite = None
         self.physics_engine = None
+        self.camera = None"""
 
         # Sets background colour for the window using arcade's provided colours
         arcade.set_background_color(arcade.csscolor.INDIGO)
@@ -50,15 +51,21 @@ class MyGame(arcade.Window):
         # Adds player sprite to "Player" sprite list
         self.scene.add_sprite("Player", self.player_sprite)
 
-        # Loop to add grass sprites in a row at equal intervals (64 pixels) to form the floor
+        # Loops to add grass & wall sprites at equal intervals (64 pixels) to form the floor and left wall
         for x in range(0, 1314, 64):
             wall = arcade.Sprite(":resources:images/tiles/grassMid.png", TILE_SCALING)
             wall.center_x = x
             wall.center_y = 32
             self.scene.add_sprite("Walls", wall)
 
+        for y in range(96, 416, 64):
+            wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", TILE_SCALING)
+            wall.center_x = 32
+            wall.center_y = y
+            self.scene.add_sprite("Walls", wall)
+
         # List of coordinates at which to place wall sprites (units are pixels)
-        coordinate_list = [[512, 96], [256, 96], [768, 96], [576, 224], [640, 224], [704, 224], [768, 224]]
+        coordinate_list = [[512, 96], [256, 96], [768, 96], [576, 224], [640, 224], [704, 224], [768, 224], [960, 416], [1152, 608], [1344, 800], [1536, 992]]
 
         # Iterates through list of coordinates and adds wall sprites at the specified locations
         for coordinate in coordinate_list:
@@ -73,10 +80,30 @@ class MyGame(arcade.Window):
             self.player_sprite, gravity_constant=GRAVITY, walls=self.scene["Walls"]
         )
 
+        # Initialises camera
+        self.camera = arcade.Camera(self.width, self.height)
+
+    def center_camera_to_player(self):
+        """"""
+
+        screen_center_x = self.player_sprite.center_x - (self.camera.viewport_width / 2)
+        screen_center_y = self.player_sprite.center_y - (self.camera.viewport_height / 2)
+
+        # Don't let camera travel past 0
+        if screen_center_x < 0:
+            screen_center_x = 0
+        if screen_center_y < 0:
+            screen_center_y = 0
+            
+        player_centered = screen_center_x, screen_center_y
+
+        self.camera.move_to(player_centered)
+
     def on_draw(self):
         """Clears and re-renders the scene every frame."""
 
         self.clear()
+        self.camera.use()
         self.scene.draw()
 
     def on_key_press(self, key, modifiers):
@@ -106,6 +133,12 @@ class MyGame(arcade.Window):
 
         # Move the player with the physics engine
         self.physics_engine.update()
+
+        # Calls center_camera_to_player method
+        self.center_camera_to_player()
+
+        if self.player_sprite.center_y < 0:
+            self.player_sprite.position = 64, 128
 
 def main():
     """Main function"""
