e2a017938c338088f457526c2e6a237ba78e385c
samdawsonWBHS <sd20428@my.westlake.school.nz>
2024-03-04 21:59:28 +1300

    Ensured tilemaps worked on home laptop - edited background layer

---
+++ simple_platformer_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="simple_platformer_tileset" tilewidth="128" tileheight="128" tilecount="90" columns="9">
+ <image source="kenney_voxel-pack/Spritesheets/spritesheet_tiles.png" width="1152" height="1280"/>
+</tileset>
