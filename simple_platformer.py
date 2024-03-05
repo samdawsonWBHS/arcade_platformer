e1b83cfb0b9d0dc6d969b61275a40ef709fefc5d
samdawsonWBHS <sd20428@my.westlake.school.nz>
2024-03-05 22:18:16 +1300

    Commented remaining code - removed faulty moving platforms

--- simple_platformer.py
+++ simple_platformer.py
@@ -20,17 +20,18 @@ COIN_SCALING = 0.5
 PLAYER_MOVEMENT_SPEED = 5
 PLAYER_JUMP_SPEED = 20
 GRAVITY = 1
-
-MAIN_PATH = os.path.dirname(os.path.abspath(__file__))
+PLAYER_SPAWN_X = 64
+PLAYER_SPAWN_Y = 128
 
 # Layer Names from our TileMap
 LAYER_NAME_FOREGROUND = "Foreground"
-LAYER_NAME_MOVING_PLATFORMS = "Moving Platforms"
 LAYER_NAME_PLATFORMS = "Platforms"
 LAYER_NAME_COINS = "Coins"
 LAYER_NAME_BACKGROUND = "Background"
 LAYER_NAME_LADDERS = "Ladders"
 LAYER_NAME_DANGERS = "Dangers"
+
+MAIN_PATH = os.path.dirname(os.path.abspath(__file__))
 
 class MyGame(arcade.Window):
     """
@@ -70,6 +71,7 @@ class MyGame(arcade.Window):
             "Platforms": {"use_spatial_hash": True,}
         }
 
+        # Sets map file name based on current level
         map_name = f"{MAIN_PATH}/map_level_{self.level}.tmx"
 
         # Loads tile map in to tile_map variable
@@ -79,14 +81,11 @@ class MyGame(arcade.Window):
         # from the map as SpriteLists in the scene in the proper order
         self.scene = arcade.Scene.from_tilemap(self.tile_map)
 
-        # Create SpriteList for wall sprites - spatial hashing improves collision detection for stationary sprites
-        self.scene.add_sprite_list("Walls", use_spatial_hash=True)
-
         # Create player_sprite as an instance of arcade's 'Sprite' class
         self.player_sprite = arcade.Sprite(":resources:images/animated_characters/male_adventurer/maleAdventurer_idle.png", CHARACTER_SCALING)
 
         # Set spawn point for player
-        self.player_sprite.position = 64, 128
+        self.player_sprite.position = PLAYER_SPAWN_X, PLAYER_SPAWN_Y
 
         # Add player sprite to "Player" sprite list
         self.scene.add_sprite("Player", self.player_sprite)
@@ -107,18 +106,22 @@ class MyGame(arcade.Window):
         if self.tile_map.background_color:
             arcade.set_background_color(self.tile_map.background_color)
 
-        # Create the 'physics engine' passing in the player sprite, gravity constant, and Platforms layer of the tile map
+        # Create the physics engine, passing in the player sprite, gravity constant, and Platforms layer of the tile map
         self.physics_engine = arcade.PhysicsEnginePlatformer(
             self.player_sprite, gravity_constant=GRAVITY, walls=self.scene["Platforms"]
         )
 
     def center_camera_to_player(self):
-        """ADD COMMENTS AND DOCSTRINGS FROM HERE"""
-
+        """
+        Update every frame to relocate camera to player.
+        """
+        
+        # Set the intended center of the screen to the coordinates of the player sprite
+        # Subtract half the viewport size in each direction since the anchor point is bottom left of screen not centre
         screen_center_x = self.player_sprite.center_x - (self.camera.viewport_width / 2)
         screen_center_y = self.player_sprite.center_y - (self.camera.viewport_height / 2)
 
-        # Don't let camera travel past 0
+        # Don't let camera travel past map boundaries
         if screen_center_x < 0:
             screen_center_x = 0
         if screen_center_x > 1280:
@@ -126,19 +129,24 @@ class MyGame(arcade.Window):
         if screen_center_y < 0:
             screen_center_y = 0
             
+        # Defines the coordinates of the camera to be centered on player
         player_centered = screen_center_x, screen_center_y
 
+        # Moves camera to correct position
         self.camera.move_to(player_centered)
 
     def on_draw(self):
         """Clears and re-renders the scene every frame."""
 
+        # Clears current frame, initialises camera for use, then renders spritelists
         self.clear()
         self.camera.use()
         self.scene.draw()
 
+        # Initialises gui_camera for use to draw the score
         self.gui_camera.use()
 
+        # Draws current score (number of diamonds collected in level)
         score_text = f"Diamonds Collected: {self.score}/3"
         arcade.draw_text(score_text, 10, 10, arcade.csscolor.WHITE, 18)
 
@@ -166,24 +174,28 @@ class MyGame(arcade.Window):
             self.player_sprite.change_x = 0
 
     def on_update(self, delta_time):
-        """Movement and game logic"""
+        """Movement and game logic - running constantly"""
 
         # Move the player with the physics engine
         self.physics_engine.update()
 
-        # Calls center_camera_to_player method
+        # Calls center_camera_to_player method to keep the player centered on screen
         self.center_camera_to_player()
 
+        # If player sprite falls below map, respawn at spawn point
         if self.player_sprite.center_y < 0:
-            self.player_sprite.position = 64, 128
+            self.player_sprite.position = PLAYER_SPAWN_X, PLAYER_SPAWN_Y
 
+        # If player sprite contacts a coin, add coin to coin_hit_list
         coin_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.scene["Coins"])
 
+        # Coins in hit list are removed from coin spritelist, while a sound is played and score increased
         for coin in coin_hit_list:
             coin.remove_from_sprite_lists()
             arcade.play_sound(self.collect_coin_sound)
             self.score += 1
 
+        # Moves to next level once score reaches 3 with all level diamonds collected - setup method called to reset everything
         if self.score == 3:
             self.level += 1
             self.reset_score = True
